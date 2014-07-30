01a5f2d25f3751a77e17cf7049259200dd06862e
Witoon <alive-@msn.com>
2014-07-30 20:57:28 +0700

    Initial project
    just initial.

---
+++ Foods Manager/frmMain.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+public class frmMain : Form
+{
+    private void InitializeComponent()
+    {
+
+    }
+    public frmMain()
+    {
+        InitializeComponent();
+    }
+    public static void Main()
+    {
+        frmMain main = new frmMain();
+        Application.Run(main);
+    }
+}
