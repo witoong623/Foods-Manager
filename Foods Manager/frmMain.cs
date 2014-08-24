168f31f8dd3c8b6426e9ca63210b43555eb07cfd
Witoon <alive-@msn.com>
2014-08-24 20:24:52 +0700

    add ingredient insert method in DBConnector class

--- Foods Manager/frmMain.cs
+++ Foods Manager/frmMain.cs
@@ -27,31 +27,32 @@ public class frmMain : Form
     private Button btnAddMaterial;
     private GroupBox gpbMaterialsHave;
 
+    #region windows component
     private void InitializeComponent()
     {
             this.gpbFoodsCanMake = new System.Windows.Forms.GroupBox();
-            this.cbFoodsCanMake = new System.Windows.Forms.ComboBox();
-            this.gpbFoodsCannotMake = new System.Windows.Forms.GroupBox();
-            this.gpbMaterialsHave = new System.Windows.Forms.GroupBox();
-            this.gpbMaterialsOut = new System.Windows.Forms.GroupBox();
-            this.cbFoodsCannotMake = new System.Windows.Forms.ComboBox();
-            this.cbMaterialsHave = new System.Windows.Forms.ComboBox();
-            this.cbMaterialsOut = new System.Windows.Forms.ComboBox();
+            this.btnAddMenu = new System.Windows.Forms.Button();
             this.lstvFoodsCanMake = new System.Windows.Forms.ListView();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.cbFoodsCanMake = new System.Windows.Forms.ComboBox();
+            this.gpbFoodsCannotMake = new System.Windows.Forms.GroupBox();
             this.lstvFoodsCannotMake = new System.Windows.Forms.ListView();
             this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.cbFoodsCannotMake = new System.Windows.Forms.ComboBox();
+            this.gpbMaterialsHave = new System.Windows.Forms.GroupBox();
+            this.btnAddMaterial = new System.Windows.Forms.Button();
             this.lstvMaterialsHave = new System.Windows.Forms.ListView();
             this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.cbMaterialsHave = new System.Windows.Forms.ComboBox();
+            this.gpbMaterialsOut = new System.Windows.Forms.GroupBox();
             this.listView1 = new System.Windows.Forms.ListView();
             this.columnHeader9 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.btnAddMenu = new System.Windows.Forms.Button();
-            this.btnAddMaterial = new System.Windows.Forms.Button();
+            this.cbMaterialsOut = new System.Windows.Forms.ComboBox();
             this.gpbFoodsCanMake.SuspendLayout();
             this.gpbFoodsCannotMake.SuspendLayout();
             this.gpbMaterialsHave.SuspendLayout();
@@ -71,77 +72,19 @@ public class frmMain : Form
             this.gpbFoodsCanMake.TabStop = false;
             this.gpbFoodsCanMake.Text = "อาหารที่ทำได้";
             // 
-            // cbFoodsCanMake
+            // btnAddMenu
             // 
-            this.cbFoodsCanMake.FormattingEnabled = true;
-            this.cbFoodsCanMake.Location = new System.Drawing.Point(123, 269);
-            this.cbFoodsCanMake.Name = "cbFoodsCanMake";
-            this.cbFoodsCanMake.Size = new System.Drawing.Size(121, 21);
-            this.cbFoodsCanMake.TabIndex = 1;
-            // 
-            // gpbFoodsCannotMake
-            // 
-            this.gpbFoodsCannotMake.Controls.Add(this.lstvFoodsCannotMake);
-            this.gpbFoodsCannotMake.Controls.Add(this.cbFoodsCannotMake);
-            this.gpbFoodsCannotMake.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.gpbFoodsCannotMake.Location = new System.Drawing.Point(280, 12);
-            this.gpbFoodsCannotMake.Name = "gpbFoodsCannotMake";
-            this.gpbFoodsCannotMake.Size = new System.Drawing.Size(344, 300);
-            this.gpbFoodsCannotMake.TabIndex = 1;
-            this.gpbFoodsCannotMake.TabStop = false;
-            this.gpbFoodsCannotMake.Text = "อาหารที่ทำไม่ได้";
-            // 
-            // gpbMaterialsHave
-            // 
-            this.gpbMaterialsHave.Controls.Add(this.btnAddMaterial);
-            this.gpbMaterialsHave.Controls.Add(this.lstvMaterialsHave);
-            this.gpbMaterialsHave.Controls.Add(this.cbMaterialsHave);
-            this.gpbMaterialsHave.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.gpbMaterialsHave.Location = new System.Drawing.Point(630, 12);
-            this.gpbMaterialsHave.Name = "gpbMaterialsHave";
-            this.gpbMaterialsHave.Size = new System.Drawing.Size(250, 300);
-            this.gpbMaterialsHave.TabIndex = 2;
-            this.gpbMaterialsHave.TabStop = false;
-            this.gpbMaterialsHave.Text = "วัตถุดิบที่มี";
-            // 
-            // gpbMaterialsOut
-            // 
-            this.gpbMaterialsOut.Controls.Add(this.listView1);
-            this.gpbMaterialsOut.Controls.Add(this.cbMaterialsOut);
-            this.gpbMaterialsOut.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.gpbMaterialsOut.Location = new System.Drawing.Point(901, 12);
-            this.gpbMaterialsOut.Name = "gpbMaterialsOut";
-            this.gpbMaterialsOut.Size = new System.Drawing.Size(146, 300);
-            this.gpbMaterialsOut.TabIndex = 3;
-            this.gpbMaterialsOut.TabStop = false;
-            this.gpbMaterialsOut.Text = "วัตถุดิบที่หมด";
-            // 
-            // cbFoodsCannotMake
-            // 
-            this.cbFoodsCannotMake.FormattingEnabled = true;
-            this.cbFoodsCannotMake.Location = new System.Drawing.Point(217, 269);
-            this.cbFoodsCannotMake.Name = "cbFoodsCannotMake";
-            this.cbFoodsCannotMake.Size = new System.Drawing.Size(121, 21);
-            this.cbFoodsCannotMake.TabIndex = 3;
-            // 
-            // cbMaterialsHave
-            // 
-            this.cbMaterialsHave.FormattingEnabled = true;
-            this.cbMaterialsHave.Location = new System.Drawing.Point(123, 269);
-            this.cbMaterialsHave.Name = "cbMaterialsHave";
-            this.cbMaterialsHave.Size = new System.Drawing.Size(121, 21);
-            this.cbMaterialsHave.TabIndex = 3;
-            // 
-            // cbMaterialsOut
-            // 
-            this.cbMaterialsOut.FormattingEnabled = true;
-            this.cbMaterialsOut.Location = new System.Drawing.Point(19, 269);
-            this.cbMaterialsOut.Name = "cbMaterialsOut";
-            this.cbMaterialsOut.Size = new System.Drawing.Size(121, 21);
-            this.cbMaterialsOut.TabIndex = 3;
+            this.btnAddMenu.Location = new System.Drawing.Point(6, 269);
+            this.btnAddMenu.Name = "btnAddMenu";
+            this.btnAddMenu.Size = new System.Drawing.Size(90, 23);
+            this.btnAddMenu.TabIndex = 6;
+            this.btnAddMenu.Text = "เพิ่มเมนูอาหาร";
+            this.btnAddMenu.UseVisualStyleBackColor = true;
+            this.btnAddMenu.Click += new System.EventHandler(this.btnAddMenu_Click);
             // 
             // lstvFoodsCanMake
             // 
+            this.lstvFoodsCanMake.Activation = System.Windows.Forms.ItemActivation.TwoClick;
             this.lstvFoodsCanMake.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader1,
             this.columnHeader2,
@@ -167,6 +110,26 @@ public class frmMain : Form
             // 
             this.columnHeader3.Text = "หน่วย";
             // 
+            // cbFoodsCanMake
+            // 
+            this.cbFoodsCanMake.FormattingEnabled = true;
+            this.cbFoodsCanMake.Location = new System.Drawing.Point(123, 269);
+            this.cbFoodsCanMake.Name = "cbFoodsCanMake";
+            this.cbFoodsCanMake.Size = new System.Drawing.Size(121, 21);
+            this.cbFoodsCanMake.TabIndex = 1;
+            // 
+            // gpbFoodsCannotMake
+            // 
+            this.gpbFoodsCannotMake.Controls.Add(this.lstvFoodsCannotMake);
+            this.gpbFoodsCannotMake.Controls.Add(this.cbFoodsCannotMake);
+            this.gpbFoodsCannotMake.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gpbFoodsCannotMake.Location = new System.Drawing.Point(280, 12);
+            this.gpbFoodsCannotMake.Name = "gpbFoodsCannotMake";
+            this.gpbFoodsCannotMake.Size = new System.Drawing.Size(344, 300);
+            this.gpbFoodsCannotMake.TabIndex = 1;
+            this.gpbFoodsCannotMake.TabStop = false;
+            this.gpbFoodsCannotMake.Text = "อาหารที่ทำไม่ได้";
+            // 
             // lstvFoodsCannotMake
             // 
             this.lstvFoodsCannotMake.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
@@ -188,6 +151,36 @@ public class frmMain : Form
             // 
             this.columnHeader5.Text = "วัตถุดิบที่ขาด";
             this.columnHeader5.Width = 210;
+            // 
+            // cbFoodsCannotMake
+            // 
+            this.cbFoodsCannotMake.FormattingEnabled = true;
+            this.cbFoodsCannotMake.Location = new System.Drawing.Point(217, 269);
+            this.cbFoodsCannotMake.Name = "cbFoodsCannotMake";
+            this.cbFoodsCannotMake.Size = new System.Drawing.Size(121, 21);
+            this.cbFoodsCannotMake.TabIndex = 3;
+            // 
+            // gpbMaterialsHave
+            // 
+            this.gpbMaterialsHave.Controls.Add(this.btnAddMaterial);
+            this.gpbMaterialsHave.Controls.Add(this.lstvMaterialsHave);
+            this.gpbMaterialsHave.Controls.Add(this.cbMaterialsHave);
+            this.gpbMaterialsHave.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gpbMaterialsHave.Location = new System.Drawing.Point(630, 12);
+            this.gpbMaterialsHave.Name = "gpbMaterialsHave";
+            this.gpbMaterialsHave.Size = new System.Drawing.Size(250, 300);
+            this.gpbMaterialsHave.TabIndex = 2;
+            this.gpbMaterialsHave.TabStop = false;
+            this.gpbMaterialsHave.Text = "วัตถุดิบที่มี";
+            // 
+            // btnAddMaterial
+            // 
+            this.btnAddMaterial.Location = new System.Drawing.Point(6, 269);
+            this.btnAddMaterial.Name = "btnAddMaterial";
+            this.btnAddMaterial.Size = new System.Drawing.Size(75, 23);
+            this.btnAddMaterial.TabIndex = 7;
+            this.btnAddMaterial.Text = "เพิ่มวัตถุดิบ";
+            this.btnAddMaterial.UseVisualStyleBackColor = true;
             // 
             // lstvMaterialsHave
             // 
@@ -216,6 +209,26 @@ public class frmMain : Form
             // 
             this.columnHeader8.Text = "หน่วย";
             // 
+            // cbMaterialsHave
+            // 
+            this.cbMaterialsHave.FormattingEnabled = true;
+            this.cbMaterialsHave.Location = new System.Drawing.Point(123, 269);
+            this.cbMaterialsHave.Name = "cbMaterialsHave";
+            this.cbMaterialsHave.Size = new System.Drawing.Size(121, 21);
+            this.cbMaterialsHave.TabIndex = 3;
+            // 
+            // gpbMaterialsOut
+            // 
+            this.gpbMaterialsOut.Controls.Add(this.listView1);
+            this.gpbMaterialsOut.Controls.Add(this.cbMaterialsOut);
+            this.gpbMaterialsOut.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gpbMaterialsOut.Location = new System.Drawing.Point(901, 12);
+            this.gpbMaterialsOut.Name = "gpbMaterialsOut";
+            this.gpbMaterialsOut.Size = new System.Drawing.Size(146, 300);
+            this.gpbMaterialsOut.TabIndex = 3;
+            this.gpbMaterialsOut.TabStop = false;
+            this.gpbMaterialsOut.Text = "วัตถุดิบที่หมด";
+            // 
             // listView1
             // 
             this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
@@ -232,28 +245,16 @@ public class frmMain : Form
             this.columnHeader9.Text = "ชื่อวัตถุดิบ";
             this.columnHeader9.Width = 110;
             // 
-            // btnAddMenu
+            // cbMaterialsOut
             // 
-            this.btnAddMenu.Location = new System.Drawing.Point(6, 269);
-            this.btnAddMenu.Name = "btnAddMenu";
-            this.btnAddMenu.Size = new System.Drawing.Size(90, 23);
-            this.btnAddMenu.TabIndex = 6;
-            this.btnAddMenu.Text = "เพิ่มเมนูอาหาร";
-            this.btnAddMenu.UseVisualStyleBackColor = true;
-            this.btnAddMenu.Click += new System.EventHandler(this.btnAddMenu_Click);
-            // 
-            // btnAddMaterial
-            // 
-            this.btnAddMaterial.Location = new System.Drawing.Point(6, 269);
-            this.btnAddMaterial.Name = "btnAddMaterial";
-            this.btnAddMaterial.Size = new System.Drawing.Size(75, 23);
-            this.btnAddMaterial.TabIndex = 7;
-            this.btnAddMaterial.Text = "เพิ่มวัตถุดิบ";
-            this.btnAddMaterial.UseVisualStyleBackColor = true;
+            this.cbMaterialsOut.FormattingEnabled = true;
+            this.cbMaterialsOut.Location = new System.Drawing.Point(19, 269);
+            this.cbMaterialsOut.Name = "cbMaterialsOut";
+            this.cbMaterialsOut.Size = new System.Drawing.Size(121, 21);
+            this.cbMaterialsOut.TabIndex = 3;
             // 
             // frmMain
             // 
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
             this.ClientSize = new System.Drawing.Size(1064, 328);
             this.Controls.Add(this.gpbMaterialsOut);
             this.Controls.Add(this.gpbMaterialsHave);
@@ -262,13 +263,19 @@ public class frmMain : Form
             this.Name = "frmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Food Manager";
-            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
             this.gpbFoodsCanMake.ResumeLayout(false);
             this.gpbFoodsCannotMake.ResumeLayout(false);
             this.gpbMaterialsHave.ResumeLayout(false);
             this.gpbMaterialsOut.ResumeLayout(false);
             this.ResumeLayout(false);
 
+    }
+    #endregion windows component
+
+    void lstvFoodsCanMake_ItemActivate(object sender, EventArgs e)
+    {
+        int id;
+        ListView.SelectedListViewItemCollection itemSelected = lstvFoodsCanMake.SelectedItems;
     }
     public frmMain()
     {
