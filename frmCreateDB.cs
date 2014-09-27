c33798fa82b7950285932159d671935688bd3905
Witoon <alive-@msn.com>
2014-09-27 21:47:24 +0700

    Build database connect form

--- frmCreateDB.cs
+++ frmCreateDB.cs
@@ -11,6 +11,11 @@ public class frmCreateDB : Form
     private TextBox txtSqlFilePath;
     private Button button1;
     private Label label4;
+    private TextBox txtSqlUsername;
+    private TextBox txtSqlPassword;
+    private TextBox txtSqlHost;
+    private Button button2;
+    private Button button3;
 
     private string FilePath;
 
@@ -23,41 +28,47 @@ public class frmCreateDB : Form
             this.label4 = new System.Windows.Forms.Label();
             this.txtSqlFilePath = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.txtSqlUsername = new System.Windows.Forms.TextBox();
+            this.txtSqlPassword = new System.Windows.Forms.TextBox();
+            this.txtSqlHost = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
-            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.label1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.label1.Location = new System.Drawing.Point(35, 40);
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 25);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(46, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "ไฟล์ .sql";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // label2
             // 
-            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.AutoSize = true;
             this.label2.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.label2.Location = new System.Drawing.Point(35, 70);
+            this.label2.Location = new System.Drawing.Point(39, 60);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(40, 13);
             this.label2.TabIndex = 1;
             this.label2.Text = "ชื่อผู้ใช้";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // label3
             // 
-            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.AutoSize = true;
             this.label3.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.label3.Location = new System.Drawing.Point(12, 100);
+            this.label3.Location = new System.Drawing.Point(33, 95);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(69, 17);
+            this.label3.Size = new System.Drawing.Size(46, 13);
             this.label3.TabIndex = 2;
             this.label3.Text = "รหัสผ่าน";
             // 
             // label4
             // 
-            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.AutoSize = true;
             this.label4.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.label4.Location = new System.Drawing.Point(12, 130);
             this.label4.Name = "label4";
@@ -67,14 +78,14 @@ public class frmCreateDB : Form
             // 
             // txtSqlFilePath
             // 
-            this.txtSqlFilePath.Location = new System.Drawing.Point(88, 40);
+            this.txtSqlFilePath.Location = new System.Drawing.Point(88, 22);
             this.txtSqlFilePath.Name = "txtSqlFilePath";
             this.txtSqlFilePath.Size = new System.Drawing.Size(179, 20);
             this.txtSqlFilePath.TabIndex = 4;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(273, 29);
+            this.button1.Location = new System.Drawing.Point(273, 23);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 5;
@@ -82,9 +93,53 @@ public class frmCreateDB : Form
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // txtSqlUsername
+            // 
+            this.txtSqlUsername.Location = new System.Drawing.Point(88, 57);
+            this.txtSqlUsername.Name = "txtSqlUsername";
+            this.txtSqlUsername.Size = new System.Drawing.Size(179, 20);
+            this.txtSqlUsername.TabIndex = 6;
+            // 
+            // txtSqlPassword
+            // 
+            this.txtSqlPassword.Location = new System.Drawing.Point(88, 92);
+            this.txtSqlPassword.Name = "txtSqlPassword";
+            this.txtSqlPassword.Size = new System.Drawing.Size(179, 20);
+            this.txtSqlPassword.TabIndex = 7;
+            // 
+            // txtSqlHost
+            // 
+            this.txtSqlHost.Location = new System.Drawing.Point(88, 127);
+            this.txtSqlHost.Name = "txtSqlHost";
+            this.txtSqlHost.Size = new System.Drawing.Size(179, 20);
+            this.txtSqlHost.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(88, 164);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "ตกลง";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(192, 164);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "ยกเลิก";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // frmCreateDB
             // 
-            this.ClientSize = new System.Drawing.Size(360, 331);
+            this.ClientSize = new System.Drawing.Size(360, 209);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtSqlHost);
+            this.Controls.Add(this.txtSqlPassword);
+            this.Controls.Add(this.txtSqlUsername);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.txtSqlFilePath);
             this.Controls.Add(this.label4);
