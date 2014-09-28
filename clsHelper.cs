b75d7cf610b70dcc4c2eb84f34d234d00d5dd706
Witoon <alive-@msn.com>
2014-09-28 08:04:57 +0700

    Change name clsConvert to clsHelper

---
+++ clsHelper.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace FoodsManager
+{
+    public enum IngredientType { Flavoring = 1, Meat, Vegetable, Fruit };
+    public enum IngredientUnit { Fong = 10, Gram, Ton, Hua, Luk, Mud, Greb };
+    public enum PreviousTask { Add, Edit };
+    /// <summary>
+    /// This class use to collect all extension method
+    /// </summary>
+    static class Convert
+    {
+        
+        /// <summary>
+        /// Convert int unit to string 
+        /// </summary>
+        /// <param name="unit">The number that indicate unit string</param>
+        /// <returns>Unit name in string format</returns>
+        public static string ToIngredientUnitString(this int unit)
+        {
+            switch (unit)
+            {
+                case 10:
+                    return "ฟอง";
+                case 11:
+                    return "กรัม";
+                case 12:
+                    return "ต้น";
+                case 13:
+                    return "หัว";
+                case 14:
+                    return "ลูก";
+                case 15:
+                    return "มัด";
+                default:
+                    return "กลีบ";
+            }
+        }
+
+        public static string ToRecipeUnitString(this int unit)
+        {
+            switch (unit)
+            {
+                case 1:
+                    return "จาน";
+                case 2:
+                    return "ชาม";
+                case 3:
+                    return "ถ้วย";
+                case 4:
+                    return "ไม้";
+                case 5:
+                    return "ถุง";
+                default:
+                    return "รหัสหน่วยผิดพลาด";
+            }
+        }
+    }
+}
